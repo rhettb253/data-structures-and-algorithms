6af7cd3fc708357ce383d0027a99b815cdcf61bf
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-07-16 16:18:22 -0700

    started assignment

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,34 @@
 'use strict';
 
-class LinkedList {
+//will create obj {value:given, next:given}
+class Node {
+  constructor(data, next=null){
+    this.data = data;
+    this.next = next;
+  }
+}
 
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+
+  //adds Node
+  insert(data) {
+    if(!this.head) this.head = new Node(data);
+    //otherwise
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  //indicates w/ boolean whether value exists elsewhere in list
+  includes(value){
+  }
+
+  //string with all LL values
+  toString(){
+  }
 }
 
 module.exports = LinkedList;
