06c23a28e0cfaded7e9305d7d21cf67e29b6259b
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-08-22 15:49:56 -0700

    tests passing

--- javascript/trees/__tests__/binaryTree.test.js
+++ javascript/trees/__tests__/binaryTree.test.js
@@ -1,10 +1,8 @@
-const {BinaryTree} = require('../trees');
-
-let emptyBiTree = new BinaryTree();
-let fullBiTree =  {value: 10, left: {value: 5, left: {value: 3, left: null, right: null}, right: {value: 7, left: null, right: null}}, right: {value: 15, left: null, right: {value: 17, left: null, right: null}}};
+const {BinarySearchTree, BinaryTree} = require('../trees');
 
 describe('binary trees', ()=> {
   test('traversal works with an empty tree', ()=> {
+    let emptyBiTree = new BinaryTree();
     let emptyAnsArrPre = emptyBiTree.preOrder(emptyBiTree.root);
     let emptyAnsArrIn = emptyBiTree.inOrder(emptyBiTree.root);
     let emptyAnsArrPost = emptyBiTree.postOrder(emptyBiTree.root);
@@ -40,5 +38,41 @@ describe('binary trees', ()=> {
     expect(ansArrPre).toEqual([10, 5, 3, 7, 15, 17]);
     // expect(ansArrIn).toEqual([]);
     // expect(ansArrPost).toEqual([]);
+  });
+  test('binary search trees can add nodes in appropriate place with one arguement', () => {
+    let biSearchTree = new BinarySearchTree(10);
+    biSearchTree.root.left = {value: 5, left: {value: 3, left: null, right: null}, right: {value: 7, left: null, right: null}};
+    biSearchTree.root.right = {value: 15, left: null, right: {value: 17, left: null, right: null}};
+    biSearchTree.add(16);
+    expect(biSearchTree.root.right.right.left.value).toEqual(16);
+  });
+  test('binary search trees can add nodes in appropriate place with two arguements', () => {
+    let biSearchTree = new BinarySearchTree(10);
+    biSearchTree.root.left = {value: 5, left: {value: 3, left: null, right: null}, right: {value: 7, left: null, right: null}};
+    biSearchTree.root.right = {value: 15, left: null, right: {value: 17, left: null, right: null}};
+    biSearchTree.add(1, biSearchTree.root);
+    expect(biSearchTree.root.left.left.left.value).toEqual(1);
+  });
+  test('binary search trees can add nodes in appropriate place: check two', () => {
+    let biSearchTree = new BinarySearchTree(10);
+    biSearchTree.root.left = {value: 5, left: {value: 3, left: null, right: null}, right: {value: 7, left: null, right: null}};
+    biSearchTree.root.right = {value: 15, left: null, right: {value: 17, left: null, right: null}};
+    biSearchTree.add(16, biSearchTree.root);
+    expect(biSearchTree.root.right.right.left.value).toEqual(16);
+  });
+  test('binary search trees can add nodes in appropriate place starting off of root', () => {
+    let biSearchTree = new BinarySearchTree(10);
+    biSearchTree.root.left = {value: 5, left: {value: 3, left: null, right: null}, right: {value: 7, left: null, right: null}};
+    biSearchTree.root.right = {value: 15, left: null, right: {value: 17, left: null, right: null}};
+    biSearchTree.add(1, biSearchTree.root.left);
+    expect(biSearchTree.root.left.left.left.value).toEqual(1);
+  });
+  test('contains method on binary search trees works', () => {
+    let biSearchTree = new BinarySearchTree(10);
+    biSearchTree.root.left = {value: 5, left: {value: 3, left: null, right: null}, right: {value: 7, left: null, right: null}};
+    biSearchTree.root.right = {value: 15, left: null, right: {value: 17, left: null, right: null}};
+    expect(biSearchTree.contains(15)).toBe(true);
+    expect(biSearchTree.contains(3)).toBe(true);
+    expect(biSearchTree.contains(100)).toBe(false);
   });
 });
