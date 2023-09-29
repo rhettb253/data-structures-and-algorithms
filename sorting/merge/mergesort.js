20c29b5637e09e3c81e2b2485ec5fc741dd24a43
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-09-29 13:57:09 -0700

    hard

--- sorting/merge/mergesort.js
+++ sorting/merge/mergesort.js
@@ -1,36 +1,52 @@
 'use strict';
 
-function mergeSort (arr) {
+function mergeSort(arr) {
   let n = arr.length;
-  if (n > 1) {
-    let mid = n/2;
-    let left = arr.slice(0, mid);
-    let right = arr.slice(mid);
-    mergeSort(left);
-    mergeSort(right);
-    mergeSort(left, right, arr);
+
+  // Base case: if the array has 0 or 1 element, it's already sorted.
+  if (n <= 1) {
+    return arr;
   }
+
+  let mid = Math.floor(n / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  // Recursively sort both halves.
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  // Merge the sorted halves.
+  return merge(left, right);
 }
 
-function merge (left, right, arr) {
+function merge(left, right) {
+  let result = [];
   let i = 0;
   let j = 0;
-  let k = 0;
+
   while (i < left.length && j < right.length) {
     if (left[i] <= right[j]) {
-      arr[k] = left[i];
-      i = i + 1;
+      result.push(left[i]);
+      i++;
     } else {
-      arr[k] = right[j];
-      j = j + 1;
+      result.push(right[j]);
+      j++;
     }
-    k = k + 1;
   }
-  if (i === left.length) {
-    right.concat(arr);
-  } else {
-    right.concat(arr);
+
+  // Add any remaining elements from both arrays.
+  while (i < left.length) {
+    result.push(left[i]);
+    i++;
   }
+
+  while (j < right.length) {
+    result.push(right[j]);
+    j++;
+  }
+
+  return result;
 }
 
 module.exports = {mergeSort, merge};
